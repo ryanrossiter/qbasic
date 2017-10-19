5f3f3d4798a3558b85fa2ab1ea03d2cabc95605c
Ryan Rossiter <nayr0788@gmail.com>
2017-10-19 16:25:50 -0400

    Change command.invoke to only accept accountType and fix WithdrawCommand

--- src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
+++ src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
@@ -1,58 +1,43 @@
 package com.queensu.cisc.qbasic.frontend.command;
+import com.queensu.cisc.qbasic.frontend.AccountManager;
+import com.queensu.cisc.qbasic.frontend.TransactionSummarizer;
+
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
 
 public class WithdrawCommand implements Command {
     private static Scanner scanner = new Scanner( System.in );
-    public String COMMAND_STRING = "withdraw"
+    public String COMMAND_STRING = "withdraw";
 
-    public void invoke(String[] params) {
-        String accNum;
-        String amount;
-        boolean noNum = true;
-        if (params.length() >0) {
-            accNum = params[0];
-        }
-        if params.length() >1){
-            amount = params[1];
-        }
-        while (noNum){
-            if (accNum == null){}
-            else if (accNum.matches("[0-9]") && accNum.length == 8){
-                if AccountsManager.Exists(params[0]){
-                    noNum = false;
-                }
-                else{
-                    System.out.print("Please enter a valid account number: ");
-                    accNum = scanner.nextLine();
-                }
-            }
-            else{
-                System.out.print("Please enter an 8-digit account number: ");
-                accNum = scanner.nextLine();
-            }
+    public boolean invoke(String accountType) {
+        Integer accNum = null;
+        Integer amount = null;
 
-        }
-        boolean noAmm = true;
-        while (noAmm){
-            if (amount == null){}
-            else if (amount.matches("[0-9]"){
-                if (Integer.parseInt(amount)<= 100000){
-                    noNum = false;
-                }
-                else{
-                    System.out.print("Amount too large, cannont withdraw over 100000: ");
-                    accNum = scanner.nextLine();
-                }
-            }
-            else{
-                System.out.print("Please enter an amount in cents: ");
-                accNum = scanner.nextLine();
-            }
+        while (accNum == null){
+            System.out.print("Enter a valid account number: ");
+            try {
+                accNum = scanner.nextInt();
+            } catch (Exception e) {}
 
+            if (AccountManager.Exists(accNum) == false) {
+                System.out.println("Account number is invalid.");
+                accNum = null;
+            }
         }
-        TransactionSummarizer.recordTransaction("WDR", accNum, amount,null,null);
+
+        while (amount == null){
+            System.out.print("Enter a valid amount to withdraw: ");
+            try {
+                accNum = scanner.nextInt();
+            } catch (Exception e) {}
+
+            if (amount != null && amount > 100000) {
+                System.out.println("Value must be less than 100000.");
+                amount = null;
+            }
+        }
+
+        TransactionSummarizer.RecordTransaction("WDR", accNum, amount, null,null);
+
+        return false;
     }
 }

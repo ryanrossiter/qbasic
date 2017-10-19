b87a172629b71dee118db2475e1c0b73f658a36d
Ryan Rossiter <nayr0788@gmail.com>
2017-10-19 14:24:50 -0400

    Add invoke method to WithDrawCommand

--- src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
+++ src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
@@ -2,4 +2,8 @@ package com.queensu.cisc.qbasic.frontend.command;
 
 public class WithdrawCommand implements Command {
     public String COMMAND_STRING = "withdraw";
+
+    public void invoke(String[] params) {
+        
+    }
 }

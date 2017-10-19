e9d24aa62ba2a5af1f9489b0a765b5282f32dbbc
Thomulus <thomulusparker@gmail.com>
2017-10-19 17:22:10 -0400

    Input
    Added Input.java

--- src/com/queensu/cisc/qbasic/frontend/Input.java
+++ src/com/queensu/cisc/qbasic/frontend/Input.java
@@ -1,55 +1,44 @@
-
+import java.util.Scanner;
 public class Input{
-    public String askForAccountNumber(same_line){
+    public String askForAccountNumber(same_line) {
+        private static Scanner scanner = new Scanner(System.in);
         //same_line is the input if the user entered all the input on one line instead of hitting enter
         noNum = true;
         String accNum = same_line;
-        while (noNum){
-            if (accNum == null){}
-            else if (accNum.matches("[0-9]") && accNum.length() == 7){
-                if (AccountsManager.Exists(params[0])){
-                    noNum = false;
-                }
-                else{
-                    System.out.print("Please enter a valid account number: ");
-                    accNum = scanner.nextLine();
-                }
-            }
-            else{
-                System.out.print("Please enter an 8-digit account number: ");
-                accNum = scanner.nextLine();
+        Integer accNum = null;
+        while (accNum == null) {
+            System.out.print("Enter a valid account number: ");
+            try {
+                accNum = scanner.nextInt();
+            } catch (Exception e) {
             }
 
+            if (AccountManager.Exists(accNum) == false) {
+                System.out.println("Account number is invalid.");
+                accNum = null;
+            }
+            }
         }
-        return accNum;
+        return accNum.toString();
     }
     public String askForAmount(same_line, login_type) {
         int max_amount;
+        Integer amount = null;
         if (login_type == machine)
             max_amount = 100000;
         else
             max_amount = 99999999;
-        boolean noAmm = true;
-        String amount = same_line;
-        while (noAmm){
-            if (amount == null){}
-            else if (amount.matches("[0-9]")){
-                if (Integer.parseInt(amount)<= max_amount){
-                    noNum = false;
-                }
-                else{
-                    System.out.print("Amount too large, please enter an amount under %d:", max_amount);
-                    accNum = scanner.nextLine();
-                }
-            }
-            else{
-                System.out.print("Please enter an amount in cents: ");
-                accNum = scanner.nextLine();
-            }
+        while (amount == null){
+            System.out.print("Enter a valid amount to withdraw: ");
+            try {
+                accNum = scanner.nextInt();
+            } catch (Exception e) {}
 
+            if ((amount != null) && (amount > max_amount)) {
+                System.out.println("Value must be less than %d", max_amount);
+                amount = null;
+            }
         }
-        TransactionSummarizer.recordTransaction("DEP", accNum, amount,null,null);
-        return false;
-    }
+        return max_amount.toString();
     }
 }
--- src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
+++ src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
@@ -1,44 +1,15 @@
 package com.queensu.cisc.qbasic.frontend.command;
 import com.queensu.cisc.qbasic.frontend.AccountManager;
 import com.queensu.cisc.qbasic.frontend.TransactionSummarizer;
+import com.queensu.cisc.qbasic.frontend.Input.java
 
 import java.util.Scanner;
 
 public class WithdrawCommand implements Command {
-    private static Scanner scanner = new Scanner( System.in );
-    public String COMMAND_STRING = "withdraw";
 
-<<<<<<< HEAD
-    public void invoke(String[] params) {
-        String accNum = null;
-        String amount = null;
-        boolean noNum = true;
-        if (0 <=params.length) {
-            accNum = params[0];
-        }
-        if (params.length >1){
-            amount = params[1];
-        }
-        while (noNum){
-            if (accNum == null){}
-            else if (accNum.matches("[0-9]") && accNum.length() == 7){
-                if (AccountsManager.Exists(params[0])){
-                    noNum = false;
-                }
-                else{
-                    System.out.print("Please enter a valid account number: ");
-                    accNum = scanner.nextLine();
-                }
-            }
-            else{
-                System.out.print("Please enter an 8-digit account number: ");
-                accNum = scanner.nextLine();
-            }
-=======
     public boolean invoke(String accountType) {
         Integer accNum = null;
         Integer amount = null;
->>>>>>> 5640f44df0fcb4f50af603f6c731697da607d623
 
         while (accNum == null){
             System.out.print("Enter a valid account number: ");

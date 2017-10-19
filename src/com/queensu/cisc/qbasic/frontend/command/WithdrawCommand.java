f9dc467d568b699aa25d71304c01443790e9e6a5
Thomulus <thomulusparker@gmail.com>
2017-10-19 15:39:17 -0400

    Withdraw Command, potentially functional
    Hi Ryan

--- src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
+++ src/com/queensu/cisc/qbasic/frontend/command/WithdrawCommand.java
@@ -8,20 +8,47 @@ public class WithdrawCommand implements Command {
         String accNum;
         String amount;
         boolean noNum = true;
-        accNum = params[0];
-        amount = params[1];
+        if (params.length() >0) {
+            accNum = params[0];
+        }
+        if params.length() >1){
+            amount = params[1];
+        }
         while (noNum){
-            if (amount == null){}
+            if (accNum == null){}
             else if (accNum.matches("[0-9]") && accNum.length == 8){
                 if AccountsManager.Exists(params[0]){
                     noNum = false;
                 }
+                else{
+                    System.out.print("Please enter a valid account number: ");
+                    accNum = scanner.nextLine();
+                }
             }
             else{
-                System.out.print("Please enter a valid account number: ");
+                System.out.print("Please enter an 8-digit account number: ");
                 accNum = scanner.nextLine();
             }
 
         }
+        boolean noAmm = true;
+        while (noAmm){
+            if (amount == null){}
+            else if (amount.matches("[0-9]"){
+                if (Integer.parseInt(amount)<= 100000){
+                    noNum = false;
+                }
+                else{
+                    System.out.print("Amount too large, cannont withdraw over 100000: ");
+                    accNum = scanner.nextLine();
+                }
+            }
+            else{
+                System.out.print("Please enter an amount in cents: ");
+                accNum = scanner.nextLine();
+            }
+
+        }
+        TransactionSummarizer.recordTransaction(COMMAND_STRING, accNum, amount);
     }
 }

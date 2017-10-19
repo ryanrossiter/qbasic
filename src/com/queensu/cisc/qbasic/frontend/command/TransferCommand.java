5640f44df0fcb4f50af603f6c731697da607d623
Ryan Rossiter <nayr0788@gmail.com>
2017-10-19 16:58:21 -0400

    Change command.invoke to only accept accountType andfix WithdrawCommand

--- src/com/queensu/cisc/qbasic/frontend/command/TransferCommand.java
+++ src/com/queensu/cisc/qbasic/frontend/command/TransferCommand.java
@@ -3,7 +3,7 @@ package com.queensu.cisc.qbasic.frontend.command;
 public class TransferCommand implements Command {
     public String COMMAND_STRING = "transfer";
 
-    public boolean invoke(String[] params) {
+    public boolean invoke(String accountType) {
         return false;
     }
 }

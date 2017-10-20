88333114baba5d8623238f8985a6d55c7eadf26d
Ryan Rossiter <nayr0788@gmail.com>
2017-10-19 20:04:30 -0400

    Add imports and missing close brace

--- src/com/queensu/cisc/qbasic/frontend/command/DeleteAcctCommand.java
+++ src/com/queensu/cisc/qbasic/frontend/command/DeleteAcctCommand.java
@@ -1,5 +1,8 @@
 package com.queensu.cisc.qbasic.frontend.command;
 
+import com.queensu.cisc.qbasic.frontend.Input;
+import com.queensu.cisc.qbasic.frontend.TransactionSummarizer;
+
 public class DeleteAcctCommand implements Command {
 
     public String getCommandString() {
@@ -7,9 +10,11 @@ public class DeleteAcctCommand implements Command {
     }
 
     public boolean invoke(String accountType) {
-        String accNumber = Input.PromptForAccountNumber("Enter the account number you want to delete: ");
-        String accName = Input.PromptForAccountName("Enter the name of the account you want to delete: )");
+        int accNumber = Input.PromptForAccountNumber("Enter the account number you want to delete: ");
+        String accName = Input.PromptForAccountName("Enter the name of the account you want to delete: ");
+
         TransactionSummarizer.RecordTransaction("DEL", accNumber, null, null, accName);
+
         return false;
     }
 }

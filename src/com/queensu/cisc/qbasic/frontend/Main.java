fd50c1d0bf33b75020eb9d14cafad84357989df4
Ryan Rossiter <nayr0788@gmail.com>
2017-10-19 20:50:47 -0400

    Add usage comment to top of Main class

--- src/com/queensu/cisc/qbasic/frontend/Main.java
+++ src/com/queensu/cisc/qbasic/frontend/Main.java
@@ -4,6 +4,11 @@ import com.queensu.cisc.qbasic.frontend.command.CommandRegistry;
 
 import java.util.Arrays;
 import java.util.Scanner;
+
+// This main class is intended to be run by the qbasic script
+// In the zip file this script is located at the root directory
+// Usage of the qbasic script: qbasic <account-filename> <transaction-summary-filename>
+
 //Main Class waits for a user to login, then keeps them in a loop which handles commands, upon logout, the main class will start
 //over and wait for another login.
 public class Main {

4d7a0d1d318095b321b4e60211b6222d52006cf5
Ryan Rossiter <nayr0788@gmail.com>
2017-10-19 20:07:41 -0400

    Fix Login to accept machine user login

--- src/com/queensu/cisc/qbasic/frontend/Login.java
+++ src/com/queensu/cisc/qbasic/frontend/Login.java
@@ -4,14 +4,14 @@ import com.queensu.cisc.qbasic.frontend.command.CommandRegistry;
 import com.queensu.cisc.qbasic.frontend.command.CommandRegistryFactory;
 
 public class Login {
-     public static CommandRegistry verify(String input){
+     public static CommandRegistry Verify(String input){
         if (input.equals("login agent")) {
+            System.out.println("Logged in as an agent user.");
             return CommandRegistryFactory.CreateAgentCommandRegistry();
-        }
-        else if (input.equals("login agent")) {
-            return CommandRegistryFactory.CreateAgentCommandRegistry();
-        }
-        else {
+        } else if (input.equals("login machine")) {
+            System.out.println("Logged in as a machine user.");
+            return CommandRegistryFactory.CreateMachineCommandRegistry();
+        } else {
             return null;
         }
      }

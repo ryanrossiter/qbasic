2a5605aab718728d484e87bf7a8bacb5da3d79c1
Thomulus <thomulusparker@gmail.com>
2017-11-16 20:36:17 -0500

    comment

--- src/com/queensu/cisc/qbasic/backend/transaction/TransferTransaction.java
+++ src/com/queensu/cisc/qbasic/backend/transaction/TransferTransaction.java
@@ -1,7 +1,7 @@
 package com.queensu.cisc.qbasic.backend.transaction;
 
 import com.queensu.cisc.qbasic.backend.AccountManager;
-//Handles the transfer transaction, receives input from TransactionRegistry.java
+//Handles the transfer transaction, receives input from TransactionRegistry.jav
 public class TransferTransaction implements Transaction {
 
     @Override
